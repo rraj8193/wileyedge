042faa49f8fe0d1e7fc1f8f1fa08809819729a8e
rraj8193 <rraj8193@gmail.com>
2023-04-21 11:24:00 +0530

    Using different ways to create object of the class

--- Practice/src/Wiley_core_java/CreateClass.java
+++ Practice/src/Wiley_core_java/CreateClass.java
@@ -1,5 +1,36 @@
 package Wiley_core_java;
 
-public class CreateClass {
+public class CreateClass implements Cloneable {
+	int a = 10;
+	void add() {
+		System.out.println("Add Method Running");
+	}
+	
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, CloneNotSupportedException {
+		
+		CreateClass cc = new CreateClass();
+		System.out.println("-------Using new Keyword--------");
+		cc.add();	
+		System.out.println(cc);
+		System.out.println();
+		
+		try {
+			Class c = Class.forName("Wiley_core_java.CreateClass");
+			CreateClass obj = (CreateClass) c.newInstance();
+			System.out.println("-------Using Class.newInstance() method--------");
+			obj.add();
+			System.out.println(obj);
+			
+			} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println();
+		System.out.println("-------Using object.Clone--------");
+		
+		CreateClass objClone = (CreateClass) cc.clone();
+		objClone.add();
+		System.out.println(objClone);
+	}
 
 }
